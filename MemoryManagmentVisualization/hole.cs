2d6842a2eac6aaee08699f6f8b514a196f33c256
Yasmine Elsayed <yasminemohamedelsayed@gmail.com>
2019-05-09 23:10:34 +0200

    simple modfications

--- MemoryManagmentVisualization/hole.cs
+++ MemoryManagmentVisualization/hole.cs
@@ -13,6 +13,8 @@ namespace MemoryManagmentVisualization
         public bool alocated;
         public String name;
         public int process_index;
+        public int hole_id;
+
         public hole(int s, int e)
         {
             start = s;
@@ -20,14 +22,15 @@ namespace MemoryManagmentVisualization
             alocated = false;
             name = "hole";
             process_index = -1;
+            hole_id = 0;
         }
-        public static void sort(List<hole> l)
+        public static void sort_by_start(List<hole> l)
         {
-            for (int i = 0; i < l.Count; i++)
+            for (int i = 0; i < l.Count - 1; i++)
             {
-                for (int j = 0; j < l.Count; j++)
+                for (int j = i + 1; j < l.Count; j++)
                 {
-                    if (l[i].start < l[j].start)
+                    if (l[i].start > l[j].start)
                     {
                         hole temp = l[i];
                         l[i] = l[j];
@@ -36,13 +39,13 @@ namespace MemoryManagmentVisualization
                 }
             }
         }
-        public static void sort2(List<hole> l)
+        public static void sort_by_size(List<hole> l)
         {
-            for (int i = 0; i < l.Count; i++)
+            for (int i = 0; i < l.Count - 1; i++)
             {
-                for (int j = 0; j < l.Count; j++)
+                for (int j = i + 1; j < l.Count; j++)
                 {
-                    if (l[i].size < l[j].size)
+                    if (l[i].size > l[j].size)
                     {
                         hole temp = l[i];
                         l[i] = l[j];
